3e89e56c71af599beda15e4907041089b037cd46
Lee Petter <txt_ch@163.com>
2020-05-03 10:54:23 +0800

    Update wsmud_plugins.user.js

--- wsmud_plugins.user.js
+++ wsmud_plugins.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wsmud_pluginss
 // @namespace    cqv1
-// @version      0.0.32.87
+// @version      0.0.32.88
 // @date         01/07/2018
 // @modified     02/05/2020
 // @homepage     https://greasyfork.org/zh-CN/scripts/371372
