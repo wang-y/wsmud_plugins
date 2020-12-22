315df84e7eafa79cb86cb410b178f561a663c340
Knva <txt_ch@163.com>
2020-12-22 13:04:00 +0800

    Update wsmud_plugins.user.js

--- wsmud_plugins.user.js
+++ wsmud_plugins.user.js
@@ -2972,7 +2972,7 @@
                         }
                     }, onelddh: function () {
                         let mlh = `// 
-                        ($f_ss)={"name":"来点动画","source":"http://ws-mud.ii74.com/gif.txt","finder":"根文件夹"}
+                        ($f_ss)={"name":"来点动画","source":"http://ii74.oss-cn-qingdao.aliyuncs.com/gif.txt","finder":"根文件夹"}
                         @js var time=Date.parse(new Date());var f=(f_ss);var n=f["name"];var s=f["source"];var fd=f["finder"];WorkflowConfig.removeWorkflow({"name":n,"type":"flow","finder":fd});$.get(s,{stamp:time},function(data,status){WorkflowConfig.createWorkflow(n,data,fd);});
                         @awiat 2000
                         tm 来点动画已导入`;
