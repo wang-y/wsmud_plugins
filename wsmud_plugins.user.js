450c8289959236c978d9186ea57de44a1e553589
Wymix <wy90314@163.com>
2021-07-17 22:17:48 +0800

    修改版本信息
    修改版本信息

--- wsmud_plugins.user.js
+++ wsmud_plugins.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         wsmud_pluginss
 // @namespace    cqv1
-// @version      0.0.32.177
+// @version      0.0.32.178
 // @date         01/07/2018
-// @modified     16/07/2021
+// @modified     17/07/2021
 // @homepage     https://greasyfork.org/zh-CN/scripts/371372
 // @description  武神传说 MUD 武神脚本 武神传说 脚本 qq群367657589
 // @author       fjcqv(源程序) & zhzhwcn(提供websocket监听)& knva(做了一些微小的贡献) &Bob.cn(raid.js作者)
